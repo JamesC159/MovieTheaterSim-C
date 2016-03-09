f273536640cd2a1ad0325875a743d983b097cea0
James Combs <Combs@cometnet-10-21-34-0.utdallas.edu>
2016-03-08 21:19:55 -0600

    Implemented parsing of movie.txt file

--- project2.c
+++ project2.c
@@ -1,15 +1,19 @@
-/* program MovieTheaterSim */
+/* Program MovieTheaterSim */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <limits.h>
 
 #define MAX_CUSTOMERS 50
 #define MAX_AGENTS 2
 #define MAX_TAKERS 1
 #define MAX_WORKERS 1
+#define DEF_ARR_S 50
 
 /********* Global Semaphores *********/
 sem_t asem;
@@ -32,6 +36,18 @@ int ticketCount = 0;
 int concessionCount = 0;
 
 /********* Queue Functions *********/
+
+/****************************************************
+ * Description: enqueue places a semaphore into the
+ * queue represented by a linked list of Nodes
+ *
+ * Parameters: *data - semaphore to place in the queue
+ *             *count - number of elements in the queue
+ *             **back - pointer to back of queue
+ *             **front - pointer to front of queue
+ *
+ * Return: nothing
+ ***************************************************/
 void enqueueSem(sem_t *data, int *count, struct semNode **back, struct semNode **front)
 {
 	if (*back == NULL)
@@ -53,6 +69,17 @@ void enqueueSem(sem_t *data, int *count, struct semNode **back, struct semNode *
 	(*count)++;
 }
 
+/****************************************************
+ * Description: removes an element from the front of
+ * the queue and updated the pointers accordingly
+ *
+ * Parameters: *count - number of elements in the queue
+ *             **back - back of queue
+ *             **front - front of queue
+ *
+ * Return: error if trying to remove from empty queue
+ * or nothing
+ ***************************************************/
 void dequeue(int *count, struct semNode **back, struct semNode **front)
 {
 	struct semNode *tempFront = (struct semNode*) malloc(1*sizeof(struct semNode));
@@ -81,6 +108,15 @@ void dequeue(int *count, struct semNode **back, struct semNode **front)
 	}
 }
 
+/****************************************************
+ * Description: empty checks to see if the queue
+ * is empty or not and prints the result
+ *
+ * Parameters: **back - back of queue
+ *             **front - front of queue
+ *
+ * Return: nothing
+ ***************************************************/
 void empty(struct semNode **back, struct semNode **front)
 {
 	if (*back == NULL && *front == NULL)
@@ -89,20 +125,37 @@ void empty(struct semNode **back, struct semNode **front)
 		printf("Queue is not empty\n");
 }
 
+/****************************************************
+ * Description: outputs the number of elements in the
+ * queue
+ *
+ * Parameters: *count - number of elements int the queue
+ *
+ * Return: nothing
+ ***************************************************/
 void queuesize(int *count)
 {
 	printf("Queue size is %d\n", *count);
 }
 
-/********* Thread Functions *********/
+/********* Thread Functions - Comments need to be updated *********/
+
+/****************************************************
+ * Description: Customer thread function to execute
+ * the actions of a customer as it goes through the
+ * theater simulation
+ *
+ * Parameters: *custID - ID of thread or customer
+ *
+ * Return: pthread_exit(NULL) if successful
+ ***************************************************/
 void* Customer(void *custID)
 {
 	int custVal, theaterVal;
 	sem_getvalue(&theaterOpen, &theaterVal);
-	sem_wait(&custsem[(int) custID]);	//Block Customers until theater is open
 	if(theaterVal == 4)
 	{
-		sem_wait(&theaterOpen);	//Decrement theaterOpen semaphore so no other customers come in.
+		sem_wait(&theaterOpen);		//Decrement theaterOpen semaphore so no other customers come in.
 		printf("Theater is now open!\n");
 		int i;
 		
@@ -116,10 +169,25 @@ void* Customer(void *custID)
 			}
 		}
 	}
+	else
+	{
+		sem_wait(&custsem[(int) custID]);	//Block Customers until theater is open
+	}
 	printf("Customer %d created\n", (int) custID);
 	pthread_exit(NULL);	
 }
 
+/****************************************************
+ * Description: This function initializes
+ * a sempahore to a given value indicated by the val
+ * paramter
+ *
+ * Parameters: *sem - semaphore to initialize
+ *             val - value to init *sem to
+ *
+ * Return: error if sem_init fails or nothing if
+ * success
+ ***************************************************/
 void* BoxOfficeAgent(void *agentID)
 {
 	printf("Box office agent %d created\n", (int) agentID);
@@ -127,6 +195,17 @@ void* BoxOfficeAgent(void *agentID)
 	pthread_exit(NULL);
 }
 
+/****************************************************
+ * Description: This function initializes
+ * a sempahore to a given value indicated by the val
+ * paramter
+ *
+ * Parameters: *sem - semaphore to initialize
+ *             val - value to init *sem to
+ *
+ * Return: error if sem_init fails or nothing if
+ * success
+ ***************************************************/
 void* TicketTaker(void *takerID)
 {
 	printf("Ticket taker %d created\n", (int) takerID);
@@ -134,6 +213,17 @@ void* TicketTaker(void *takerID)
 	pthread_exit(NULL);
 }
 
+/****************************************************
+ * Description: This function initializes
+ * a sempahore to a given value indicated by the val
+ * paramter
+ *
+ * Parameters: *sem - semaphore to initialize
+ *             val - value to init *sem to
+ *
+ * Return: error if sem_init fails or nothing if
+ * success
+ ***************************************************/
 void* ConcessionStandWorker(void *workerID)
 {
 	printf("Concession stand worker %d created\n", (int) workerID);
@@ -142,6 +232,18 @@ void* ConcessionStandWorker(void *workerID)
 }
 
 /********* Functions *********/
+
+/****************************************************
+ * Description: This function initializes
+ * a sempahore to a given value indicated by the val
+ * paramter
+ *
+ * Parameters: *sem - semaphore to initialize
+ *             val - value to init *sem to
+ *
+ * Return: error if sem_init fails or nothing if
+ * success
+ ***************************************************/
 void initSems(sem_t *sem, int val)
 {
 	if (sem_init(sem, 0, val) == -1)
@@ -149,9 +251,16 @@ void initSems(sem_t *sem, int val)
 		perror("SEM_INIT_ERROR:");
 		exit(1);
 	}
-	return;
 }
 
+/****************************************************
+ * Description: Checks to see if there was an error
+ * when creating or joining threads
+ *
+ * Parameters: *rc - return from pthread_create
+ *
+ * Return: error if pthread_create fails or nothing
+ ***************************************************/
 void checkThreadError(int *rc)
 {
 	if (*rc)
@@ -161,12 +270,102 @@ void checkThreadError(int *rc)
 	}
 }
 
+/****************************************************
+ * Description: Opens movie.txt file 
+ *
+ * Parameters: **file - movie.txt file pointer
+ *
+ * Return: error if could not open file or nothing
+ ***************************************************/
+void openFile(FILE **file)
+{
+	*file = fopen("movies.txt", "r");
+	if (*file == NULL)
+	{
+		perror("ERROR: cannot open file");
+		exit(1);
+	}
+}
+
+/****************************************************
+ * Description: Parses the file for movie titles and
+ * the corresponding ticket amounts for each movie
+ * title
+ *
+ * Parameters: **file - movies.txt file pointer
+ *             ***movieTitles - pointer to array of string
+ *             	to hold the movie titles
+ *             **ticketCount - pointer to array of ints to
+ *             hold the number of tickets for each movie
+ *
+ * Return: error if strtol fails or nothing
+ ***************************************************/
+void parseFile(FILE **file, char ***movieTitles, int **ticketCount)
+{
+	int i = 0, j = 0;        //Counters for array access
+	char *line = NULL;       //Line to get from the file
+	size_t numBytes = 100;   //Number of bytes to read from the file for a line
+	ssize_t read;            //Number of bytes read form a line in the file
+	char *token, *endptr;    //Token pointer for string token and enptr for use in strtol
+	char *delim = "\t\n";    //Delimiters used to signal a token
+	int tokenCounter = 0;    //Counter used to determine if the token is a movie name
+	                         //Or the number of tickets for the corresponding movie
+	int base = 10;           //Base to convert ticket numbers to
+
+	while((read = getline(&line, &numBytes, *file)) != -1) 
+	{
+		token = strtok(line, delim);
+		while(token != NULL)
+		{
+			if (tokenCounter % 2 == 0)
+			{
+				strcpy((*movieTitles)[i], token);
+				i++;
+			}
+			else
+			{
+				(*ticketCount)[j] = (int)strtol(token, &endptr, base);
+				
+				errno = 0; //distingush success/failure after call
+
+				/* Check for various errors */
+				if (errno == ERANGE && ((*ticketCount)[j] == LONG_MAX 
+					|| (*ticketCount)[j] == LONG_MIN)
+					|| (errno != 0 && (*ticketCount)[j] == 0))
+				{
+					perror("strtol");
+					exit(1);
+				}
+
+				if (endptr == token)
+				{
+					fprintf(stderr, "No digits were found\n");
+					exit(1);
+				}
+				
+				/* If we got here, strtol successfully parsed a number */
+				j++;
+			}
+
+			tokenCounter++;
+			token = strtok(NULL, delim);
+		}
+	}
+
+	/* free dynamic memory */
+	if (line)
+		free(line);
+}
+
 /********* Beginning main function *********/
 int main(int argc, char **argv)
 {	
-	int i;		//Loop Counter
+	int i, j;	//Loop Counter
 	int rc; 	//Error checker for threads
 	void *status; 	//Status of joins
+	FILE *file;	//Pointer to movies.txt file
+	char **movieTitles = (char **) malloc(DEF_ARR_S*sizeof(char)); //Char ** to hold movie titles
+	int *ticketCount = (int *) malloc (DEF_ARR_S*sizeof(int)); //int * to hold ticket counts to movie titles.
 
 	pthread_t agents[MAX_AGENTS];		// Box Office Agents threads
 	pthread_t customers[MAX_CUSTOMERS];	// Customers threads
@@ -175,15 +374,27 @@ int main(int argc, char **argv)
 
 	/* Initialize all semaphores */
 	initSems(&theaterOpen, 0);
-	initSems(&tsem, MAX_TAKERS);
-	initSems(&csem, MAX_WORKERS);
-	initSems(&asem, MAX_AGENTS);
+//	initSems(&tsem, MAX_TAKERS);
+//	initSems(&csem, MAX_WORKERS);
+//	initSems(&asem, MAX_AGENTS);
 	for (i = 0; i < MAX_CUSTOMERS; i++)
 	{
 		initSems(&custsem[i], 0);	
 	}
 
-	/* First create BoxOfficeAgentsm TickerTaker, and ConcessionStandWorker threads */
+ 	/* Parse movies.txt file to figure out the movie theater titles and ticket counts */	
+	for (i = 0; i < DEF_ARR_S; i++) //First allocate space for array of pointers
+	{
+		movieTitles[i] = (char*) malloc(DEF_ARR_S*sizeof(char));
+	}
+	openFile(&file); //Now open and parse file
+	parseFile(&file, &movieTitles, &ticketCount);
+	
+
+	/* First create BoxOfficeAgentsm TickerTaker, and ConcessionStandWorker threads 
+	 * I would create a function for this procedure, but passing a function name as
+	 * shown below is a little more tricky than I though. I was thinking function pointers
+	 * would work, but am not familiar enough with them to use them yet. */
 	for (i = 0; i < MAX_AGENTS; i++)
 	{
 		rc = pthread_create(&agents[i], NULL, BoxOfficeAgent, (void *) i);
@@ -214,6 +425,14 @@ int main(int argc, char **argv)
 		printf("Customer %d joined with status %d\n", i, (int) status);
 	}
 	
+	/* free dynamic memory */
+	for (i = 0; i < DEF_ARR_S; i++)
+	{
+		free(movieTitles[i]);
+	}
+	free(movieTitles);
+	free(ticketCount);
+
 	/* Exit main thread */
 	pthread_exit(NULL);
 
